a39ba60a571950754f37c37acabb5c2a6c32237d
Gustavo Aroeira <gustavo.aroeira@gmail.com>
2023-03-13 12:11:15 -0400

    New disordered mode conv data

--- mode_convergence/disorder/run.sh
+++ mode_convergence/disorder/run.sh
@@ -1,9 +1,8 @@
-mkdir Nc75
-mkdir Nc100
-cp Nc50/input.jl Nc75/
-cp Nc50/input.jl Nc100/
-sed -i 's/Nc = 50/Nc = 75/g' Nc75/input.jl
-sed -i 's/Nc = 50/Nc = 100/g' Nc100/input.jl
+for N in 0 1 5 10; do
+    mkdir Nc$N
+    cp Nc50/input.jl Nc$N/
+    sed -i "s/Nc = 50/Nc = $N/g" Nc$N/input.jl
+done
 grep 'Nc = ' */input.jl
 echo '------------------'
 grep 'σM =' */input.jl
