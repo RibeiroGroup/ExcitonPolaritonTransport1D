5f1ffae88e372c57e14658db81d86a43f774bb2f
Gustavo Aroeira <gustavo.aroeira@gmail.com>
2023-03-20 12:11:25 -0400

    New mode conv data with different rabi splitting

--- mode_convergence/disorder/run.sh
+++
@@ -1,9 +0,0 @@
-for N in 20 35; do
-    mkdir Nc$N
-    cp Nc50/input.jl Nc$N/
-    sed -i "s/Nc = 50/Nc = $N/g" Nc$N/input.jl
-done
-grep 'Nc = ' */input.jl
-echo '------------------'
-grep 'σM =' */input.jl
-
